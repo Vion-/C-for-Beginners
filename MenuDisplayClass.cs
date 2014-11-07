75cc84e0d832d223527bd54d0bcd73512e4b31b9
Vion- <bryan19929@msn.com>
2014-11-07 15:37:50 -0600

    Adding Menu Display portion of code

---
+++ MenuDisplayClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class MenuDisplayClass
+    {
+        public void MainMenu ()
+        {
+            Console.Clear();
+            Console.WriteLine("Main Menu \n=========================\n");
+            Console.WriteLine("1. Insert Vehicle Details \n2. Under Construction \n3. Under Construction \n4. Exit");
+            Console.WriteLine("\nGo to option:  ");
+        }
+        public void VehicleTypeMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose Vehicle Type \n=========================\n");
+            Console.WriteLine("1. Car \n2. Motorbike \n3. Lorrie \n4. Cancel");
+            Console.WriteLine("\nOption:  ");
+        }
+
+        public void ExitMessage ()
+        {
+            Console.Clear();
+            Console.WriteLine("Thank you for using our system!");
+            Console.ReadLine();
+        }
+        public void OptionNotAvailableMessage()
+        {
+            Console.Clear();
+            Console.WriteLine("Option not available");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //-----------------------------------------------------------------------
+                //-----------------------------------------------------------------------
+                //Beginning of Menu display
+                //-----------------------------------------------------------------------
+                //-----------------------------------------------------------------------
+
+                //used to navigate menu options
+                byte menu = 0;
+
+                if (login == "Check Passed!")
+                {
+                    //class used to display different menus 
+                    MenuDisplayClass menuDisplay = new MenuDisplayClass();
+                    //option 4 = exit program
+                    while (menu != 4)
+                    {
+                        menuDisplay.MainMenu();
+                        menu = byte.Parse(Console.ReadLine());
+                        switch (menu)
+                        {
+                            case 1:
+                                menuDisplay.VehicleTypeMenu();
+                                menu = byte.Parse(Console.ReadLine());
+                                switch (menu)
+                                {
+                                    case 1:
+                                        break;
+                                    case 2:
+                                        break;
+                                    case 3:
+                                        break;
+                                    default:
+                                        menu = 0;
+                                        break;
+                                }
+                                break;
+                            case 4:
+                                menuDisplay.ExitMessage();
+                                break;
+                            default:
+                                menuDisplay.OptionNotAvailableMessage();
+                                break;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Clear();
+                Console.WriteLine("Oops! Something went wrong: \n\n {0}", e);
+                Console.ReadLine();
+            }
+        }
+    }
+}
